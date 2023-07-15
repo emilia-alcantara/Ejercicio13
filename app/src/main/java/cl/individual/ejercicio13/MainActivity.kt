ffd9ba41b95bc3ccb0884d0754269dd8a7ed01eb
Emilia <correoalcantaraemilia@gmail.com>
2023-07-14 21:25:44 -0400

    Parte A

--- app/src/main/java/cl/individual/ejercicio13/MainActivity.kt
+++ app/src/main/java/cl/individual/ejercicio13/MainActivity.kt
@@ -2,6 +2,10 @@ package cl.individual.ejercicio13
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.text.NumberFormat
+import java.util.Currency
+import kotlin.math.ceil
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,3 +13,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+fun main() {
+    var resultado = suma(5.5, 6.4F)
+    println(resultado)
+
+    var resultRound = redondear(7.6)
+    println(resultRound)
+
+    var moneda = mostrarResultado(8.57)
+    println(moneda)
+}
+
+fun suma (param1: Double, param2:Float) : Double = param1 + param2
+
+fun redondear (param1: Double) = ceil(param1)
+
+fun mostrarResultado(numero: Double) : String {
+
+    val format: NumberFormat = NumberFormat.getCurrencyInstance()
+    format.setMaximumFractionDigits(2)
+    format.setCurrency(Currency.getInstance("EUR"))
+    return format.format(numero)
+
+}
+
+
